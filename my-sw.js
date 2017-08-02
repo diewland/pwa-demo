e7631d5ff47370592a25642cadc8e07dec871013
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-08-02 15:45:51 +0700

    test remove theme_color from manifest

--- my-sw.js
+++ my-sw.js
@@ -1,6 +1,6 @@
 importScripts('./sw-toolbox/sw-toolbox.js');
 
-// 20170802 - change app theme
+// 20170802 - remove theme_color from manifest
 
 // cache during service-worker install step
 toolbox.precache([
