9c2764f9a3b14de04082333ece3a32b489c171c8
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-08-02 17:49:10 +0700

    change app theme color to #070

--- my-sw.js
+++ my-sw.js
@@ -1,6 +1,6 @@
 importScripts('./sw-toolbox/sw-toolbox.js');
 
-// 20170802 - remove theme_color from manifest
+// 20170802 - change app theme to #070
 
 // cache during service-worker install step
 toolbox.precache([
@@ -11,5 +11,5 @@ toolbox.router.get('css/*', toolbox.networkFirst);
 toolbox.router.get('img/*', toolbox.networkFirst);
 
 // cache during service-worker active step
-toolbox.router.get('', toolbox.networkFirst);
+toolbox.router.get('', toolbox.networkFirst); // index
 toolbox.router.get('js/*', toolbox.networkFirst);
